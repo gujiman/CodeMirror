d6f88d1589dc269ad2620548524e117ba4ed0c39
gujiman <gujiman@hotmail.com>
2015-10-05 03:51:27 +0100

    better css hints
    better css hints with control-space

--- addon/hint/css-hint.js
+++ addon/hint/css-hint.js
@@ -25,6 +25,7 @@
               to: CodeMirror.Pos(cur.line, token.end)};
 
     var start = token.start, end = cur.ch, word = token.string.slice(0, end - start);
+
     if (/[^\w$_-]/.test(word)) {
       word = ""; start = end = cur.ch;
     }
@@ -37,15 +38,58 @@
         if (!word || name.lastIndexOf(word, 0) == 0)
           result.push(name);
     }
+    function getPropertyName(){
+      var getFullLine = cm.doc.getLine(cm.doc.getCursor().line);
+      var getPartLine = getFullLine.substring(0, cur.ch);
+      var getSeparator = getPartLine.lastIndexOf(":");
+      getPartLine = getPartLine.substring(0, getSeparator);
+      getPartLine = getPartLine.replace(/{/g, '{ ').replace(/;/g, '; ').replace(/\s\s+/g, ' ');
+      getPartLine = " " + getPartLine.replace(/^\s+|\s+$/gm,'');
+      return getPartLine.split(" ").splice(-1)[0];
+   }
+   function myKeySet(array) {
+     var keys = {};
+     for (var i = 0; i < array.length; ++i) {
+       var val = array[i];
+       if (val.indexOf("<") == -1){
+         keys[val] = true;
+       }else{
+         var commonCssVals = spec.commonCssValues[val].split(",");
+         for (var x = 0; x < commonCssVals.length; ++x) {
+            keys[commonCssVals[x]] = true;
+         }
+       }
+     }
+     return keys;
+   }
 
     var st = inner.state.state;
+
     if (st == "pseudo" || token.type == "variable-3") {
       add(pseudoClasses);
     } else if (st == "block" || st == "maybeprop") {
       add(spec.propertyKeywords);
-    } else if (st == "prop" || st == "parens" || st == "at" || st == "params") {
+    } else if (st == "parens" || st == "at" || st == "params") {
       add(spec.valueKeywords);
       add(spec.colorKeywords);
+    } else if (st == "prop") {
+       
+      var getProp = getPropertyName();
+      var getPropValues = spec.cssProperties[getProp];
+      
+      if (typeof getPropValues !== "undefined"){
+         var keywords = {};
+         if (getPropValues != ""){
+            keywords = myKeySet(getPropValues.split(","));
+         }
+         keywords["inherit"] = true;
+         keywords["initial"] = true;
+         add(keywords);
+         
+      }else{
+         add(spec.valueKeywords);
+         add(spec.colorKeywords);
+      }
     } else if (st == "media" || st == "media_parens") {
       add(spec.mediaTypes);
       add(spec.mediaFeatures);
